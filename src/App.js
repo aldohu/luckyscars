ecf0eabcfbd4f74ac2fb33543bafb18a77689e32
Aldin Huric <alhuric89@gmail.com>
2025-08-07 13:44:16 +0200

    Added styling and sceleton of app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
+import React from 'react';
+// Assuming you have a CSS file for styles
+import './categories.styles.scss'; // Assuming you have a CSS file for categories styles
+
 const App = () => {
-	return <div className="App">Hello World!</div>;
+	const categories = [
+		{
+			title: 'hatchbacks',
+			id: 1,
+			image:
+				'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+		},
+		{
+			title: 'sedans',
+			id: 2,
+			image:
+				'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+		},
+		{
+			title: 'suv',
+			id: 3,
+			image:
+				'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+		},
+		{
+			title: 'crossover',
+			id: 4,
+			image:
+				'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+		},
+		{
+			title: 'roadster',
+			id: 5,
+			image:
+				'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+		},
+	];
+	return (
+		<div className="categories-container">
+			{categories.map(({ title, id }) => (
+				<div
+					className="category-container"
+					key={id}
+				>
+					<div className="background-image" />
+					<div className="category-body-container">
+						<h2 className="category-title">{title}</h2>
+						<p>Buy now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
